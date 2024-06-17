39073aa1e07e6bd8e861c70647a5982581b2e731
Kevin Gimbel <kevin@gimbel.dev>
2024-06-17 09:53:47 +0200

    fun: Add decode to caesar-cipher.hs

--- 01_fun/caesar-cipher.hs
+++ 01_fun/caesar-cipher.hs
@@ -1,13 +1,19 @@
 -- ceaser cipher
 --
 -- write out text based on a fixed "shift" in letters
---
---
 
-shiftLetter :: Char -> Int -> Char
-shiftLetter ' ' _ = ' '
-shiftLetter letter 0 = letter
-shiftLetter letter offset = shiftLetter (succ letter) (offset - 1)
+-- shiftLetter performs a caesar cipher on a letter. 
+-- Char -> Letter to shift
+-- Int  -> Amount to shift by
+-- Book -> Direction (True -> Encode, False -> Decode) 
+shiftLetter :: Char -> Int -> Bool -> Char
+shiftLetter ' ' _ _ = ' '
+shiftLetter letter 0 _ = letter
+shiftLetter letter offset True = shiftLetter (succ letter) (offset - 1) True
+shiftLetter letter offset False = shiftLetter (pred letter) (offset - 1) False
 
 encode :: [Char] -> Int -> [Char]
-encode input offset = [ shiftLetter x offset | x <- input ]
+encode input offset = [ shiftLetter x offset True | x <- input ]
+
+decode :: [Char] -> Int -> [Char]
+decode input offset = [ shiftLetter x offset False | x <- input ]
